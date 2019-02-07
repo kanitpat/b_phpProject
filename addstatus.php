8a0601721c3ace445ae3994345098ce99fc16667
kanitpat <32385232+kanitpat@users.noreply.github.com>
2019-02-08 02:48:06 +0700

    addstatus

--- addstatus.php
+++ addstatus.php
@@ -2,21 +2,21 @@
 
 session_start();
 require 'dbconnect.php';
-//     $userid = $_SESSION['userid'];
+    $userid = $_SESSION['userid'];
 
-// if (!empty($_GET['status'])) {
-//     $status = $_GET['status'];
-//     if ($status == 0) {
-//         $sql = "INSERT INTO `statuses`( `numstatus`, `detail`,`date`, `time`, `idUser`)
-//         VALUES ( '$status','ปิด',NOW(),NOW(),'$userid')";
-//         $result_page = mysqli_query($connect, $sql, MYSQLI_STORE_RESULT) or die(mysqli_error($connect));
-//     } else {
-//         $sql = "INSERT INTO `statuses`(`numstatus`, `detail`,`date`, `time`, `idUser`)
-//         VALUES
-//         ( ('$status'),'เปิด',NOW(),NOW(),'$userid')";
-//         $result_page = mysqli_query($connect, $sql, MYSQLI_STORE_RESULT) or die(mysqli_error($connect));
-//     }
-// }
+if (!empty($_GET['status'])) {
+    $status = $_GET['status'];
+    if ($status == 0) {
+        $sql = "INSERT INTO `statuses`( `numstatus`, `detail`,`date`, `time`, `idUser`)
+        VALUES ( '$status','ปิด',NOW(),NOW(),'$userid')";
+        $result_page = mysqli_query($connect, $sql, MYSQLI_STORE_RESULT) or die(mysqli_error($connect));
+    } else {
+        $sql = "INSERT INTO `statuses`(`numstatus`, `detail`,`date`, `time`, `idUser`)
+        VALUES
+        ( ('$status'),'เปิด',NOW(),NOW(),'$userid')";
+        $result_page = mysqli_query($connect, $sql, MYSQLI_STORE_RESULT) or die(mysqli_error($connect));
+    }
+}
 if (!empty($_GET['status']) && !empty($_GET['iduser'])) {
     $status = $_GET['status'];
     $iduser = $_GET['iduser'];
